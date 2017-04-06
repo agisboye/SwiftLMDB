4301eab886d6ef539e3f695fccddff3ec482b1c6
August <august@heegaard.org>
2017-04-06 12:42:42 +0200

    Move to new CLMDB repo
    This repo includes LMDB as a submodule, making it easy to update and
    maintain.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftLMDB",
     dependencies: [
-      .Package(url: "https://github.com/SUIRON/CLMDB.git", majorVersion: 0, minor: 0),
-      ]
+      .Package(url: "https://github.com/agisboye/CLMDB.git", majorVersion: 0, minor: 1),
+    ]
 )
