5b6b28edd9f872e3f4529786d830eff4a16c1e32
agisboye <august@heegaard.org>
2024-12-14 00:26:15 +0100

    Update to LMDB 0.9.33

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/agisboye/CLMDB.git", exact: "0.9.31")
+        .package(url: "https://github.com/agisboye/CLMDB.git", exact: "0.9.33")
     ],
 
     targets: [
